54511fcd20825cc0bfa7475a299b8588c09a12ea
Jack <jhulatt7@gmail.com>
2023-07-10 03:12:44 +0100

    Updated the GUI to look a little less ancient

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 def calculate(operation):
     num1 = float(entry1.get())
@@ -21,30 +22,47 @@ def calculate(operation):
     result_label.config(text=str(result))
 
 root = tk.Tk()
+root.title("Calculator")
 
-label1 = tk.Label(root, text="Enter first number: ")
-label1.pack()
-entry1 = tk.Entry(root)
-entry1.pack()
+style = ttk.Style()
+style.configure("TButton",
+                foreground="midnight blue",
+                background="light sky blue",
+                font=("Arial", 16),
+                padding=10)
+style.configure("TEntry",
+                foreground="midnight blue",
+                font=("Arial", 16),
+                padding=10)
+style.configure("TLabel",
+                foreground="midnight blue",
+                background="alice blue",
+                font=("Arial", 16),
+                padding=10)
 
-label2 = tk.Label(root, text="Enter second number: ")
-label2.pack()
-entry2 = tk.Entry(root)
-entry2.pack()
+label1 = ttk.Label(root, text="Enter first number: ")
+label1.grid(row=0, column=0, padx=10, pady=10)
+entry1 = ttk.Entry(root)
+entry1.grid(row=0, column=1, padx=10, pady=10)
 
-add_button = tk.Button(root, text='+', command=lambda: calculate('+'))
-add_button.pack()
+label2 = ttk.Label(root, text="Enter second number: ")
+label2.grid(row=1, column=0, padx=10, pady=10)
+entry2 = ttk.Entry(root)
+entry2.grid(row=1, column=1, padx=10, pady=10)
 
-subtract_button = tk.Button(root, text='-', command=lambda: calculate('-'))
-subtract_button.pack()
+add_button = ttk.Button(root, text='+', command=lambda: calculate('+'))
+add_button.grid(row=2, column=0, padx=10, pady=10)
 
-multiply_button = tk.Button(root, text='*', command=lambda: calculate('*'))
-multiply_button.pack()
+subtract_button = ttk.Button(root, text='-', command=lambda: calculate('-'))
+subtract_button.grid(row=2, column=1, padx=10, pady=10)
 
-divide_button = tk.Button(root, text='/', command=lambda: calculate('/'))
-divide_button.pack()
+multiply_button = ttk.Button(root, text='*', command=lambda: calculate('*'))
+multiply_button.grid(row=3, column=0, padx=10, pady=10)
 
-result_label = tk.Label(root, text="Result: ")
-result_label.pack()
+divide_button = ttk.Button(root, text='/', command=lambda: calculate('/'))
+divide_button.grid(row=3, column=1, padx=10, pady=10)
+
+result_label = ttk.Label(root, text="Result: ")
+result_label.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
 
 root.mainloop()
